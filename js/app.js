68e503f157b206c1299e5c72640ec039dbcd8c33
Pijush Sarker <pijushsarker.dev@gmail.com>
2023-02-18 15:46:57 +0600

    done with ellipse

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ const setAreaToResultField = (areaNameId,area) => {
     }
 }
 
-const getAreaFromInput = (side1Id,side2Id,hasFraction) => {
+const getAreaFromInput = (side1Id,side2Id,hasFraction,isEllipse) => {
     const side1 = document.getElementById(side1Id);
     const side2 = document.getElementById(side2Id);
     const side1String = side1.value;
@@ -25,6 +25,12 @@ const getAreaFromInput = (side1Id,side2Id,hasFraction) => {
     if((sideNo1 > 0 && sideNo2 > 0) && (typeof sideNo1 === 'number' && typeof sideNo2 === 'number')){
         if(hasFraction){
             area = 0.5 * sideNo1 * sideNo2;
+            const areaS = area.toFixed(2);
+            const areaFinal = parseFloat(areaS);
+            return areaFinal;
+        }
+        else if(isEllipse){
+            area = 3.1416 * sideNo1 * sideNo2;
             const areaS = area.toFixed(2);
             const areaFinal = parseFloat(areaS);
             return areaFinal;
@@ -46,7 +52,7 @@ const toggleInput = (elementId) => {
 
 }
 
-const getAreaFromElement = (side1Id,side2Id,hasFraction) => {
+const getAreaFromElement = (side1Id,side2Id,hasFraction,isEllipse) => {
     const side1 = document.getElementById(side1Id);
     const side2 = document.getElementById(side2Id);
     const side1String = side1.innerText;
@@ -62,6 +68,12 @@ const getAreaFromElement = (side1Id,side2Id,hasFraction) => {
         const areaFinal = parseFloat(areaS);
         return areaFinal;
     }
+    else if(isEllipse){
+        area = 3.1416 * sideNo1 * sideNo2;
+        const areaS = area.toFixed(2);
+        const areaFinal = parseFloat(areaS);
+        return areaFinal;
+    }
     else{
         area = sideNo1 * sideNo2;
         const areaS = area.toFixed(2);
@@ -70,28 +82,28 @@ const getAreaFromElement = (side1Id,side2Id,hasFraction) => {
     } 
 }
 
-
+//add click event to the button
 const cards = document.querySelectorAll('.cards');
 cards.forEach((card) => {
     card.addEventListener('click', (event) => {
         const targetElement = event.target.classList;
         
         if(targetElement.contains('triangle-btn')){
-            const area = getAreaFromInput('triangle-side1', 'triangle-side2', true);
+            const area = getAreaFromInput('triangle-side1', 'triangle-side2', true,false);
             setAreaToResultField('triangle',area);
 
         }
         else if(targetElement.contains('rectangle-btn')){
-            const area = getAreaFromInput('rectangle-side1', 'rectangle-side2', false);
+            const area = getAreaFromInput('rectangle-side1', 'rectangle-side2', false,false);
             setAreaToResultField('rectangle',area);
         }
         else if(targetElement.contains('parallelogram-btn')){
             const checkField = document.getElementById('check-parallelogram');
             if(checkField.checked){
-                const area = getAreaFromInput('parallelogram-side1', 'parallelogram-side2', false);
+                const area = getAreaFromInput('parallelogram-side1', 'parallelogram-side2', false,false);
                 setAreaToResultField('parallelogram',area);
             }else{
-                const area = getAreaFromElement('parallelogram-side1-element', 'parallelogram-side2-element', false);
+                const area = getAreaFromElement('parallelogram-side1-element', 'parallelogram-side2-element', false,false);
                 setAreaToResultField('parallelogram',area);
             }
             
@@ -99,30 +111,30 @@ cards.forEach((card) => {
         else if(targetElement.contains('rhombus-btn')){
             const checkField = document.getElementById('check-rhombus');
             if(checkField.checked){
-                const area = getAreaFromInput('rhombus-side1', 'rhombus-side2', true);
+                const area = getAreaFromInput('rhombus-side1', 'rhombus-side2', true,false);
                 setAreaToResultField('rhombus',area);
             }else{
-                const area = getAreaFromElement('rhombus-side1-element', 'rhombus-side2-element', true);
+                const area = getAreaFromElement('rhombus-side1-element', 'rhombus-side2-element', true,false);
                 setAreaToResultField('rhombus',area);
             }
         }
         else if(targetElement.contains('pentagon-btn')){
             const checkField = document.getElementById('check-pentagon');
             if(checkField.checked){
-                const area = getAreaFromInput('pentagon-side1', 'pentagon-side2', true);
+                const area = getAreaFromInput('pentagon-side1', 'pentagon-side2', true,false);
                 setAreaToResultField('pentagon',area);
             }else{
-                const area = getAreaFromElement('pentagon-side1-element', 'pentagon-side2-element', true);
+                const area = getAreaFromElement('pentagon-side1-element', 'pentagon-side2-element', true,false);
                 setAreaToResultField('pentagon',area);
             }
         }
         else if(targetElement.contains('ellipse-btn')){
             const checkField = document.getElementById('check-ellipse');
             if(checkField.checked){
-                const area = getAreaFromInput('ellipse-side1', 'ellipse-side2', false);
+                const area = getAreaFromInput('ellipse-side1', 'ellipse-side2', false,true);
                 setAreaToResultField('ellipse',area);
             }else{
-                const area = getAreaFromElement('ellipse-side1-element', 'ellipse-side2-element', false);
+                const area = getAreaFromElement('ellipse-side1-element', 'ellipse-side2-element', false,true);
                 setAreaToResultField('ellipse',area);
             }
         }
@@ -143,22 +155,4 @@ cards.forEach((card) => {
         
 
     })
-    // card.addEventListener('toggle', (event) => {
-    //     const targetElement = event.target.classList;
-    //     if(targetElement.contains('edit-parallelogram')){
-    //         toggleInput('input-parallelogram');
-    //         console.log('edit clicked');
-
-
-    //     }
-    //     else if(targetElement.contains('edit-rhombus')){
-    //         toggleInput('input-rhombus');
-    //     }
-    //     else if(targetElement.contains('edit-pentagon')){
-    //         toggleInput('input-pentagon');
-    //     }
-    //     else if(targetElement.contains('edit-ellipse')){
-    //         toggleInput('input-ellipse');
-    //     }
-    // })
 })
